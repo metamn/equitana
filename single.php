97d0d3a375e140d3064600d053401c9a3799f8cf
rubycs <rubycs@clair.ro>
2010-06-23 18:29:29 +0300

    Single product page integrated with wp-e-commerce and related posts

--- single.php
+++ single.php
@@ -17,50 +17,50 @@ get_header();
 		</div>
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<h1><?php the_title(); ?></h1>
-
-			<div class="entry">
-				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
-
-				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-				<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
-
-				<p class="postmetadata alt">
-					<small>
-						This entry was posted
-						<?php /* This is commented, because it requires a little adjusting sometimes.
-							You'll need to download this plugin, and follow the instructions:
-							http://binarybonsai.com/wordpress/time-since/ */
-							/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?>
-						on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
-						and is filed under <?php the_category(', ') ?>.
-						You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
-
-						<?php if ( comments_open() && pings_open() ) {
-							// Both Comments and Pings are open ?>
-							You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
-
-						<?php } elseif ( !comments_open() && pings_open() ) {
-							// Only Pings are Open ?>
-							Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.
-
-						<?php } elseif ( comments_open() && !pings_open() ) {
-							// Comments are open, Pings are not ?>
-							You can skip to the end and leave a response. Pinging is currently not allowed.
-
-						<?php } elseif ( !comments_open() && !pings_open() ) {
-							// Neither Comments, nor Pings are open ?>
-							Both comments and pings are currently closed.
-
-						<?php } edit_post_link('Edit this entry','','.'); ?>
-
-					</small>
-				</p>
-
-			</div>
+		  <div class='block'>
+		    <div id="info" class="column span-14 last">
+		      <h1><?php the_title(); ?></h1>
+		      <div class='entry'>
+            <?php the_content(); ?>
+          </div> 
+		    </div>
+		    <div id="shopping" class="column span-9 last">
+		      <?php 
+            $product_id = get_post_meta($post->ID, 'product_id', single);
+            if ($product_id) {        
+              echo wpsc_display_products_page('product_id='.$product_id);         
+            }      
+          ?>
+		    </div>
+		  </div>
+			
+			<div class='postmetadata'>
+			  Publicat <?php the_time('l, j F, Y') ?>  
+        <br/>
+        <?php the_tags('Etichete: ', ', ', '<br />'); ?> 
+        Categorii <?php the_category(', ') ?> 
+        <br/>
+        Adresa <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> |
+        Adresa <?php post_comments_feed_link('RSS 2.0'); ?> pentru comentarii
+        <?php edit_post_link('Modificare articol',' | ','.')?>
+			</div>			
 		</div>
 
-	<?php comments_template(); ?>
+    <div id="recommended">    
+     <?php
+        $related_posts = MRP_get_related_posts($post->ID, true);
+        if ($related_posts) { ?>        
+          <h3>Produse similare</h3>
+          <?php foreach ($related_posts as $post) {
+            setup_postdata($post);            
+            include "product-list.php";
+          }
+        } 
+      ?>
+    </div>
+    <div class='clearfix'></div>
+
+	  <?php comments_template(); ?>
 
 	<?php endwhile; else: ?>
 
