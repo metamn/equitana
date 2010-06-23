af2f7f05d2a83dd126654be58320c326be5c53f5
rubycs <rubycs@clair.ro>
2010-06-23 12:09:40 +0300

    Start integrating wp-e-commerce

--- sidebar.php
+++ sidebar.php
@@ -20,5 +20,20 @@
       </ul>
     </li>
   </ul>   
+  <?php global $wpsc_query; ?> 
+    <ul class='wpsc_categories'>
+			<?php wpsc_start_category_query(array('category_group'=>get_option('wpsc_default_category'), 'show_thumbnails'=> get_option('show_category_thumbnails'))); ?>
+					<li>
+						<?php wpsc_print_category_image(32, 32); ?>
+						
+						<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_link"><?php wpsc_print_category_name();?></a>
+						<?php if(get_option('wpsc_category_description')) :?>
+							<?php wpsc_print_category_description("<div class='wpsc_subcategory'>", "</div>"); ?>				
+						<?php endif;?>
+						
+						<?php wpsc_print_subcategory("<ul>", "</ul>"); ?>
+					</li>
+			<?php wpsc_end_category_query(); ?>
+		</ul>
 </div>
 <div class='triangle'></div> 
