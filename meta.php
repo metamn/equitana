8ce050929d67c7c11c24d7fc334b782a9ad031d0
rubycs <rubycs@clair.ro>
2010-07-21 14:37:13 +0300

    Meta products styled

--- meta.php
+++ meta.php
@@ -1,16 +1,28 @@
 <?php get_header(); ?>
 
-<div id="content" class="meta column span-24 last">  
-  <?php if (have_posts()) :	while (have_posts()) : the_post(); ?>
-      <div id="product" class="thumb column span-8 last">    
-        <?php include "product-single.php";  ?> 
-      </div> 
-    <?php endwhile;
-    echo '<div class="clearfix"></div>';      
-    include "navigation.php";  
-  else :
-    include "not-found.php";
-  endif; ?>    
+<div id="content" class="meta column span-24 last"> 
+  <h1><?php single_cat_title(); ?></h1> 
+  <?php 
+    $i = 0;
+    if (have_posts()) :	
+      while (have_posts()) : the_post(); ?>
+        <div id="product" class="thumb column span-7 prepend-1 last">    
+          <?php include "product-single.php";  ?> 
+        </div>
+        <?php 
+          $i = $i + 1;
+          if (($i % 3) == 0) {
+            echo '<hr/>';
+          }
+        ?> 
+      <?php endwhile;
+    
+      echo '<div class="clearfix"></div>';      
+      include "navigation.php";  
+    
+    else :
+      include "not-found.php";
+    endif; ?>    
 </div>
 
 <?php get_footer(); ?>
